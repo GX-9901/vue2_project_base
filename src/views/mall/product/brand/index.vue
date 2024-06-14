e487603e58d464a0efc7c058deae2726557da74f
rx78_2 <gx-9901-dx@foxmail.com>
2024-06-14 15:32:37 +0800

    商品品牌

--- src/views/mall/product/brand/index.vue
+++ src/views/mall/product/brand/index.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>ProductBrand</div>
+  <div class="product-brand">
+    <el-card class="box-card" shadow="never">
+
+      <el-form inline >
+        <el-form-item label="品牌名称">
+          <el-input v-model="input" placeholder="请输入品牌名称"></el-input>
+        </el-form-item>
+        <el-form-item label="状态">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              label="开启"
+              value="0">
+            </el-option>
+            <el-option
+              label="关闭"
+              value="1">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="创建时间">
+          <div class="block">
+            <el-date-picker
+              v-model="value1"
+              type="daterange"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期">
+            </el-date-picker>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+          <el-button icon="el-icon-refresh">重置</el-button>
+        </el-form-item>
+      </el-form>
+      <div class="brand-toolbox">
+        <el-button icon="el-icon-plus" type="primary" plain>新增</el-button>
+        <el-row :gutter="10">
+          <el-button icon="el-icon-search" circle></el-button>
+          <el-button icon="el-icon-refresh" circle></el-button>
+        </el-row>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,4 +51,16 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.brand-toolbox{
+  display: flex;
+  justify-content: space-between;
+}
+.product-brand {
+  padding: 20px;
+}
+
+//.brand-date-picker {
+//  width: 250px;
+//}
+</style>
---
+++ src/views/mall/product/kohaku/index.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<template>
+<div>
+  123
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
